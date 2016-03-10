26abd82da367b83f86918be2e41c9b57b340d8b5
Pedro Alberto Bazan Serrano <pedroalberto007@gmail.com>
2016-03-09 18:48:34 -0600

    Comenzando con el copy paste

--- views/promotionconf-productos/promotionconf-productos.js
+++ views/promotionconf-productos/promotionconf-productos.js
@@ -1,7 +1,43 @@
 ({
+	events:{
+		'keyup textarea[name="paste"]': '_handlerPaste'
+	},
+	initialize: function (argument) {
+		this._super('initialize', arguments);
+		this.meta = {
+			fields: [
+				{
+					name: 'todos_usuario_c',
+					type: 'bool',
+					label: 'LBL_TODOS_USUARIO'
+				},
+			]
+		}
+		this.model = app.data.createBean('QS_Promociones');
+		this.action = 'detail';
+		this.collection = app.data.createBeanCollection('QS_ProductosCriterio');
+		this.collection.on('reset add remove', _.bind(this._renderCollection, this))
+	},
+
 	_renderHtml: function(){
+		this._super('_renderHtml', arguments);
 		var self = this;
-		self.currentView = app.template.get('promotionconf-productos.QS_Promociones');
-		self.$el.html(self.currentView());
+	},
+
+	_handlerPaste: function(evt) {
+		var self = this;
+		var text = evt.target.value;
+		var rows = text.split('\n');
+		var model = null;
+		_.each(rows, function(row, index) {
+			model = app.data.createBean('QS_ProductosCriterio', {name: row});
+			self.collection.add(model)
+		})
+	},
+
+	_renderCollection: function () {
+		var $container = this.$el.find('.productos-criterio-grid');
+		var template = app.template.get('promotionconf-productos.productos-criterio-grid.QS_Promociones');
+		$container.html(template(this)); 
 	}
 })
