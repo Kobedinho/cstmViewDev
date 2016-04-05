4e2478a3cb180deeac14defe8832b263ab2361e9
Pedro Alberto Bazan Serrano <pedroalberto007@gmail.com>
2016-04-05 13:14:54 -0500

    ajuste a listado de arbol para productos participantes

---
+++ qs-helpers.js
@@ -0,0 +1,48 @@
+/**
+ * Handlebars helpers.
+ *
+ * These functions are to be used in handlebars templates.
+ * @class Handlebars.helpers
+ * @singleton
+ */
+(function(app) {
+    app.events.on("app:init", function() {
+        /**
+         * convert a string to upper case
+         */
+        Handlebars.registerHelper("ppArbol", function (text)
+        {   
+            //debugger;
+            var arbol = '<ul class="pp-arbol grupos">';
+            //debugger;
+            function getNodo(nodoData, nodoString) {
+                nodoString = '<li class="group" data-grupo="'+nodoData.grupo+'">' 
+                + '<div class="header">' + (nodoData.iniciador ? '<span class="label iniciador">Iniciador</span>' : '')+'<a href="#">'+nodoData.name+'</a></div>'
+                + '<div class="condicion">'+nodoData.condicion+'</div> '
+                + '<a class="btn agregar-grupo">Agregar grupo</a>';
+                if(nodoData.grupos.length){
+                    nodoString += '<ul class="grupos">';
+                    _.each(nodoData.grupos, function (nodoHijoData) {
+                        nodoString += getNodo(nodoHijoData, '');
+                    });
+                    nodoString += '</ul>';
+                }
+                else{
+                    nodoString += '<div class="sin-grupos">Sin grupos hijos</div>';    
+                }
+                nodoString += '</li>';
+                return nodoString;
+            }
+            //debugger;
+            _.each(this.arbol, function (nodoData) {
+               arbol += getNodo(nodoData, '');
+            });
+            //return new Handlebars.SafeString('<ul class="arbol"><li class="group"><div><a href="#">Nombre de grupo 1</a></div><div>Condiciones mayor a x y unidad de medida</div>  <span class="label">Iniciador</span><a class="btn">Agregar grupo</a><ul><li class="group"><div><a href="#">Nombre de grupo 1-1</a></div><div>Condiciones mayor a x y unidad de medida</div> <a class="btn">Agregar grupo</a><ul></ul><div>Sin grupos hijos</div></li><li class="group"><div><a href="#">Nombre de grupo 1-2</a></div><div>Condiciones mayor a x y unidad de medida</div>    <a class="btn">Agregar grupo</a><ul></ul><div>Sin grupos hijos</div></li></ul><div class="hidden">Sin grupos hijos</div></li><li class="group"><div><a href="#">Nombre de grupo 2</a></div><div>Condiciones mayor a x y unidad de medida</div>  <span class="label">Iniciador</span><a class="btn">Agregar grupo</a><ul></ul><div>Sin grupos hijos</div></li></ul>');
+            //arbol += getNodo();
+            arbol += '</ul>';
+            return new Handlebars.SafeString(arbol);
+        });
+
+
+    });
+})(SUGAR.App); 
