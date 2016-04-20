8e1634241ba8d865489a40b36a07754f3ce738dc
Pedro Alberto Bazan Serrano <pedroalberto007@gmail.com>
2016-04-19 20:09:38 -0500

    Pasando la promo a la edición de productos de criterio

--- views/promotionconf-productos-group-list/promotionconf-productos-group-list.js
+++ views/promotionconf-productos-group-list/promotionconf-productos-group-list.js
@@ -10,6 +10,7 @@
 		this.collection.on('reset add remove', _.bind(this.render, this));
 		this.grupos = {};
 		this.arbol = [];
+		debugger;
 	},
 
 	_handlerCreateGroup: function (argument) {
@@ -26,7 +27,7 @@
 		var nodoPadre = grupoPadre ? grupoPadre.nodo : null;
 		var grupo = (nodoPadre ? nodoPadre.grupo + '-' : '') + ((nodoPadre ? nodoPadre.grupos:this.arbol).length+1).toString();
 		var iniciador = nodoPadre ? false : true;
-
+		debugger;
 		this.groupEditView =  app.view.createView({
 			type: 'promotionconf-productos-edit-group',
 			name: 'promotionconf-productos-edit-group',
@@ -35,7 +36,8 @@
 			productoCriterio: {
                 grupo_c: grupo,
                 grupo_padre_c: this.grupoPadre ,
-                iniciador_c: iniciador
+                iniciador_c: iniciador,
+                qs_productoscriterio_qs_promocionesqs_promociones_ida: this.model.id
             }
 		});
 		this.groupEditView.on('onSave', _.bind(this._handlerSaveGrupo, this));
